4f195405ff6a5051eeb8301bc9d82eb56e6840d5
dalkkommi <dalkkommi@gmail.com>
2021-10-04 19:09:17 +0900

    PlantList.java 파일 생성 후 xml 연결

---
+++ app/src/main/java/com/cookandroid/plantandroid/PlantList.java
@@ -0,0 +1,12 @@
+package com.cookandroid.plantandroid;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PlantList extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.plant_list);
+    }
+}
