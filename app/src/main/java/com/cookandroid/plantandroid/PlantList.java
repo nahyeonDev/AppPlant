560e0feb642cb4ea522a92c6003f371e9e5250f7
dalkkommi <dalkkommi@gmail.com>
2021-10-09 03:15:03 +0900

    recyclerview에 버튼 아이템 추가에 필요한 코드 업데이트 - 아직 형식만 갖춤

--- app/src/main/java/com/cookandroid/plantandroid/PlantList.java
+++ app/src/main/java/com/cookandroid/plantandroid/PlantList.java
@@ -1,15 +1,23 @@
 package com.cookandroid.plantandroid;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ClipData;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import java.util.ArrayList;
+import java.util.List;
 
 public class PlantList extends AppCompatActivity{
     private ImageButton backBtn_plant_list;
+    private RecyclerView RecyclerView_main = null;
+    private PlantAdapter pAdapter = null;
+    private ArrayList<ListItemObj> pList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,7 +27,7 @@ public class PlantList extends AppCompatActivity{
         //10.5추가
         backBtn_plant_list= findViewById(R.id.backBtn_plant_list);
 
-        //식물 상세페이지 연결
+//        식물 상세페이지 연결
 //        listBtn1.setOnClickListener(new Button.OnClickListener(){
 //            @Override
 //            public void onClick(View v) {
@@ -36,6 +44,25 @@ public class PlantList extends AppCompatActivity{
             public void onClick(View v) {finish();}
         });
 
+        RecyclerView_main = findViewById(R.id.recycler_view);
+        pList = new ArrayList<>();
 
+        pAdapter = new PlantAdapter(pList);
+        RecyclerView_main.setAdapter(pAdapter);
+        RecyclerView_main.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL,false));
+
+        addItem("식물1");
+        addItem("2");
+        addItem("3");
+
+
+        pAdapter.notifyDataSetChanged();
+
+    }
+
+    private void addItem(String text) {
+        ListItemObj item = new ListItemObj(text);
+
+        pList.add(item);
     }
 }
