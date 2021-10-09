f0f37fe94d397323fd65e65aa373f64b15e2c4f2
nahyeonDev <nahyeon.dev@gmail.com>
2021-10-09 16:11:11 +0900

    item_recycler.xml 모양 변경, 구글 로그인 버튼 추가/ 연결은 할 예정.

--- app/src/main/java/com/cookandroid/plantandroid/PlantList.java
+++ app/src/main/java/com/cookandroid/plantandroid/PlantList.java
@@ -1,17 +1,22 @@
 package com.cookandroid.plantandroid;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.content.ClipData;
-import android.content.Intent;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.util.Log;
 import android.widget.ImageButton;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
-import java.util.List;
 
 public class PlantList extends AppCompatActivity{
     private ImageButton backBtn_plant_list;
@@ -22,47 +27,38 @@ public class PlantList extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.plant_list);
+            //10.5추가
+            backBtn_plant_list = findViewById(R.id.backBtn_plant_list);
 
-        //10.5추가
-        backBtn_plant_list= findViewById(R.id.backBtn_plant_list);
-
+            //식물 상세페이지 연결
 //        식물 상세페이지 연결
 //        listBtn1.setOnClickListener(new Button.OnClickListener(){
 //            @Override
 //            public void onClick(View v) {
-//                Intent intent = new Intent(getApplicationContext(), PlantDetail.class);
-//                startActivity(intent);
-//            }
-//        });
+//            protected void onCreate(Bundle savedInstanceState) {
+//                public void onClick(View v) {finish();}
+//            });
+
+            RecyclerView_main = findViewById(R.id.recycler_view);
+            pList = new ArrayList<>();
+
+            pAdapter = new PlantAdapter(pList);
+            RecyclerView_main.setAdapter(pAdapter);
+            RecyclerView_main.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL, false));
+
+            addItem("식물1");
+            addItem("2");
+            addItem("3");
 
 
+            pAdapter.notifyDataSetChanged();
 
-        //뒤로가기 버튼
-        backBtn_plant_list.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View v) {finish();}
-        });
+        }
 
-        RecyclerView_main = findViewById(R.id.recycler_view);
-        pList = new ArrayList<>();
+        private void addItem (String text){
+            ListItemObj item = new ListItemObj(text);
 
-        pAdapter = new PlantAdapter(pList);
-        RecyclerView_main.setAdapter(pAdapter);
-        RecyclerView_main.setLayoutManager(new LinearLayoutManager(this, RecyclerView.HORIZONTAL,false));
-
-        addItem("식물1");
-        addItem("2");
-        addItem("3");
-
-
-        pAdapter.notifyDataSetChanged();
+            pList.add(item);
+        }
 
     }
-
-    private void addItem(String text) {
-        ListItemObj item = new ListItemObj(text);
-
-        pList.add(item);
-    }
-}
